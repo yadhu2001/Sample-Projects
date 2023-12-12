d2b415a05f00d1efc892110f6df3dcc4be2fa3cd
Yadhu <kyadhu653@gmail.com>
2023-12-12 08:48:35 +0530

    todo delete option update

--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -1,29 +1,42 @@
 from django.shortcuts import render
 from . forms import Todoform
 from .models import Todo
-
+ 
 # Create your views here.
 
 def index(request):
     context={}
+    form=Todoform()
 
 
     if request.method=='POST':
-        if 'submit' in request.POST:
-            todo_form=Todoform(request.POST)
-            todo_form.save()
+        if 'save' in request.POST:
+            key=request.POST.get('save')
 
-        if 'delete' in request.POST:
+            if not key:
+                form=Todoform(request.POST)
+            else:
+                print(key)
+                todo_edit=Todo.objects.get(id=key)
+                form=Todoform(request.POST, instance=todo_edit)
+
+
+            form.save()
+            form=Todoform
+            
+        elif 'delete' in request.POST:
             key=request.POST.get('delete')
             todo=Todo.objects.get(id=key)
             todo.delete()
 
+        elif 'edit' in request.POST:
+            key=request.POST.get('edit')
+            todo_edit=Todo.objects.get(id=key)
+            form=Todoform(instance=todo_edit)
+
+    todo=Todo.objects.all
 
 
-    todo_form=Todoform()
-    todo=Todo.objects.all()
-
-    context['todo_form']=todo_form
+    context['form']=form
     context['todo']=todo
-
-    return render(request,'index.html',context)
+    return render (request,'index.html',context)
